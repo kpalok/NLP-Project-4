47802674c2c56aac6b5a6bcb070fc9204e3b6520
Kalle Palokangas <kalle.palokangas@pinja.com>
2022-10-26 13:21:09 +0300

    10-equal subdivision for Task 10

--- Task10.py
+++ Task10.py
@@ -40,6 +40,21 @@ def calc_LD(lines):
 
     return np.array(lines_LD)
 
+def calc_10_equal_subdivision(LD_list):
+    bin_val = (np.max(LD_list) - np.min(LD_list)) / 10
+
+    bins = []
+    counts = []
+
+    for i in range(10):
+        bins.append(bin_val * (i + 1))
+        if i == 0:
+            counts.append(len([ld for ld in LD_list if ld >= bin_val * i and ld <= bin_val * (i + 1)]))
+        else:
+            counts.append(len([ld for ld in LD_list if ld > bin_val * i and ld <= bin_val * (i + 1)]))
+
+    return bins, counts
+
 CG_lines = read_poem_lines('ChildsGarden.txt')
 CG_LD = calc_LD(CG_lines)
 np.savetxt('Task 10 Results/ChildsGarden_LD.csv', np.vstack((np.arange(len(CG_LD)), CG_LD)).T, delimiter=',')
@@ -56,6 +71,17 @@ ax[0].bar(np.arange(len(CG_LD)), CG_LD)
 
 ax[1].set_title('The Prophet', fontsize=8)
 ax[1].bar(np.arange(len(P_LD)), P_LD)
+
+fig, ax = plt.subplots(2, 1)
+fig.suptitle('10-equal subdivision of LD histograms', fontsize=12)
+
+ax[0].set_title('Childrens Garden of Verses', fontsize=8)
+bins, counts = calc_10_equal_subdivision(CG_LD)
+ax[0].bar(bins, counts)
+
+ax[1].set_title('The Prophet', fontsize=8)
+bins, counts = calc_10_equal_subdivision(P_LD)
+ax[1].bar(bins, counts)
 
 plt.show()
 
